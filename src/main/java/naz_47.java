6d4a5098cfb208e6836a50dedc8104665e1cab93
Shaikh Mohammed Moinuddin <s.moinuddin2310@gmail.com>
2024-10-09 21:19:10 +0530

    Initialization

---
+++ src/main/java/naz_47.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;  
+import org.openqa.selenium.chrome.ChromeDriver;  
+  
+public class naz_47 {  
+  
+    public static void main(String[] args) {  
+        
+    // declaration and instantiation of objects/variables  
+    WebDriver driver=new ChromeDriver();  
+      
+    driver.navigate().to("https://github.com/Codesmith-23");  
+          
+    // Click on the search text box and send value  
+  
+      
+    }  
+  
+}  
